b5540dbbf2470d018a4411c7bc64e060358370c6
Simanto Hasan <simanto848@gmail.com>
2025-02-18 15:38:30 +0600

    create Firend model with relationships

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Friend extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'friend_id',
+        'status'
+    ];
+
+    public function user1() {
+        return $this->belongsTo(User::class, 'user1_id');
+    }
+
+    public function user2() {
+        return $this->belongsTo(User::class, 'user2_id');
+    }
 }
